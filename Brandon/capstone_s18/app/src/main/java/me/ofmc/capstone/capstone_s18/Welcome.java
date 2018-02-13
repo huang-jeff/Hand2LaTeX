ecf0a68f63f304ebc38d142e944381df16e6a883
Brandon S <atomicbcs@gmail.com>
2018-02-13 10:46:46 -0500

    Tutorial App works

--- Brandon/capstone_s18/app/src/main/java/me/ofmc/capstone/capstone_s18/Welcome.java
+++ Brandon/capstone_s18/app/src/main/java/me/ofmc/capstone/capstone_s18/Welcome.java
@@ -1,13 +1,25 @@
 package me.ofmc.capstone.capstone_s18;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Welcome extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "me.ofmc.capstone.capstone_s18.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
